6a8987b08da585ee6a2e77c615bc6a706ebdf1ec
Milan Borovy <milan.borovy@bisimulations.com>
2024-12-16 20:15:25 +0100

    Fix base unit creation

--- mb/units/temperature.lua
+++ mb/units/temperature.lua
@@ -4,7 +4,7 @@ local Type = require "mb.units.base.type"
 --- Temperature units.
 local Temperature = Type.new()
 
-Temperature:register_base(Unit.BASE ~ "K") -- Define Kelvin as a base unit.
+Temperature:register_base(Unit.BASE .. "K") -- Define Kelvin as a base unit.
 Temperature:register(Temperature["K"] - 273.15 .. "*C") -- Celsius.
 Temperature:register(Temperature["K"] * 1.8 .. "*R") -- Rankine.
 Temperature:register(Temperature["R"] - 459.67 .. "*F") -- Fahrenheit.
